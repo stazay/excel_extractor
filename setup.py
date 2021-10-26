742d6e17422a368a415e13abb4e853ff8456071b
Saba Tazayoni <61740634+stazay@users.noreply.github.com>
2021-10-26 11:33:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='excel_extractor',
-    version='1.00',
+    version='1.0',
     author='Saba Tazayoni',
     author_email="",
     description="A package to create relational databases via Microsoft Excel.",
     url="https://github.com/stazay/excel_extractor",
     packages=setuptools.find_packages(),
     install_requires=['datetime',
-                      'sys',
                       'xlsxwriter',
                       'xlwings'], 
     python_requires='>=3.6',
