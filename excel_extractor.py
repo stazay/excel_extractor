4400074597ae3c3fbd55b29814f55aa678568e5c
Saba Tazayoni <61740634+stazay@users.noreply.github.com>
2021-12-10 14:00:26 +0000

    Update excel_extractor.py
    Simplified function names.

--- excel_extractor.py
+++ excel_extractor.py
@@ -1,6 +1,5 @@
 """
 excel_extractor - a basic relational database creator       - Saba Tazayoni, 15/10/2021
-
 The purpose of this code is to be able to consolidate data from Excel spreadsheets. 
 A further function allows it to then form a relational database - and extract data from other spreadsheets 
     where there is a matching point of reference.
@@ -17,17 +16,18 @@ import xlwings as xw
 # LOOSE FUNCTIONS
 # LOOSE FUNCTIONS
 def check_total_rows(workbook, sheet_index):
-    ### THIS FUNCTION CHECKS NUMBER OF ROWS IN THE WORKSHEET.
+    # THIS FUNCTION CHECKS NUMBER OF ROWS IN THE WORKSHEET.
     # workbook          -- the workbook being queried
     # sheet_index       -- the worksheet being queried
     worksheet = workbook.sheets[sheet_index]
-    last_row = worksheet.range('A' + str(worksheet.cells.last_cell.row)).end('up').row
-    
+    last_row = worksheet.range(
+        'A' + str(worksheet.cells.last_cell.row)).end('up').row
+
     return last_row
 
 
 def check_row_number(workbook, sheet_index, queried_column, queried_datum):
-    ### THIS FUNCTION LOOKS FOR CORRESPONDING DATA WITHIN THE QUERIED COLUMN, AND RETURNS A ROW NUMBER WITH A MATCH.
+    # THIS FUNCTION LOOKS FOR CORRESPONDING DATA WITHIN THE QUERIED COLUMN, AND RETURNS A ROW NUMBER WITH A MATCH.
     # workbook          -- the workbook being queried
     # sheet_index       -- the worksheet being queried
     # queried_column    -- the column of information that data is being extracted from
@@ -36,7 +36,8 @@ def check_row_number(workbook, sheet_index, queried_column, queried_datum):
 
     try:
         for row_number in range(1, last_row):
-            queried_cell = workbook.sheets[sheet_index].range(f"{queried_column}{row_number}").value
+            queried_cell = workbook.sheets[sheet_index].range(
+                f"{queried_column}{row_number}").value
             if (queried_datum == queried_cell):
                 return row_number
     except:
@@ -54,12 +55,12 @@ def clean_datetime_object(input, format):
             return output
         except:
             return input
-    
+
     return input
 
 
 def define_backups(workbook, sheet_index, desired_columns, queried_df_index, queried_column):
-    ### THIS FUNCTION IS USED TO DEFINE BACKUPS ARGUMENT, IF REQUIRED, FOR EXTRACT_ADDITIONAL_DATA_TO_DF()
+    # THIS FUNCTION IS USED TO DEFINE BACKUPS ARGUMENT, IF REQUIRED, FOR EXTEND_DF_ENTRIES()
     # workbook          -- the workbook being queried
     # sheet_index       -- the worksheet being queried
     # desired_columns   -- a list containing all of the columns from which to extract data from
@@ -68,28 +69,30 @@ def define_backups(workbook, sheet_index, desired_columns, queried_df_index, que
     #                       eg: 3                   will take i[3] from the entry in the dataframe, and search for a match within the queried_column
     # queried_column    -- the column of information that is being queried for a match against the queried_datum
     try:
-        backups = [workbook, sheet_index, desired_columns, queried_df_index, queried_column]
-        return backups      
+        backups = [workbook, sheet_index, desired_columns,
+                   queried_df_index, queried_column]
+        return backups
     except:
         print("Backups not configured in usable format.")
         sys.exit(1)
 
 
 def extract_datum(workbook, sheet_index, queried_column, queried_row):
-    ### THIS FUNCTION SEARCHES FOR CORRESPONDING DATA WITHIN THE QUERIED CELL, AND RETURNS IT AS AN OUTPUT.
+    # THIS FUNCTION SEARCHES FOR CORRESPONDING DATA WITHIN THE QUERIED CELL, AND RETURNS IT AS AN OUTPUT.
     # workbook          -- the workbook being queried
     # sheet_index       -- the worksheet being queried
     # queried_column    -- the column of information that data is being extracted from
     # queried_row       -- the row of information that data is being extracted from
-    output = workbook.sheets[sheet_index].range(f"{queried_column}{queried_row}").value
+    output = workbook.sheets[sheet_index].range(
+        f"{queried_column}{queried_row}").value
 
     return output
 
 
 # LARGER FUNCTIONS
 # LARGER FUNCTIONS
-def extract_new_entries_to_df(df, workbook, sheet_index, desired_columns, queried_rows="default", clean_datetime=False, print_statements=True):
-    ### THIS FUNCTION IS USED TO CREATE THE BASIS OF YOUR DATAFRAME; ALL ENTRIES WITHIN THE RANGE ARE WRITTEN TO THE DATAFRAME
+def create_df_entries(df, workbook, sheet_index, desired_columns, queried_rows="default", clean_datetime=False, print_statements=True):
+    # THIS FUNCTION IS USED TO CREATE THE BASIS OF YOUR DATAFRAME; ALL ENTRIES WITHIN THE RANGE ARE WRITTEN TO THE DATAFRAME
     # df                -- the dataframe being written
     # workbook          -- the workbook being queried
     # sheet_index       -- the worksheet being queried
@@ -107,18 +110,21 @@ def extract_new_entries_to_df(df, workbook, sheet_index, desired_columns, querie
         try:
             (first_row, last_row) = queried_rows
         except:
-            print(f"Queried Range: {queried_rows} must be a TUPLE, containing only TWO numbers")
+            print(
+                f"Queried Range: {queried_rows} must be a TUPLE, containing only TWO numbers")
             sys.exit(1)
-        
+
     try:
         for i in range(first_row, last_row):
             row_data = []
             for column in desired_columns:
-                output_data = extract_datum(workbook, sheet_index, f"{column}", i)
-                
+                output_data = extract_datum(
+                    workbook, sheet_index, f"{column}", i)
+
                 # clean the datetime objects
                 if (clean_datetime != False):
-                    output_data = clean_datetime_object(output_data, clean_datetime)
+                    output_data = clean_datetime_object(
+                        output_data, clean_datetime)
 
                 row_data.append(output_data)
             df.append(row_data)
@@ -126,20 +132,22 @@ def extract_new_entries_to_df(df, workbook, sheet_index, desired_columns, querie
             # print progress
             if print_statements:
                 progress = int((i / last_row) * 100)
-                print(f"Extracting data from {workbook}: {progress}% -- {row_data}")
+                print(
+                    f"Extracting data from {workbook}: {progress}% -- {row_data}")
 
         if print_statements:
             print(f"Extracting data from {workbook}: 100%")
         return df
 
     except:
-        print(f"Queried Range: {queried_rows} must be a TUPLE, containing only TWO numbers, whereby the second number is bigger than the first number")
+        print(
+            f"Queried Range: {queried_rows} must be a TUPLE, containing only TWO numbers, whereby the second number is bigger than the first number")
         sys.exit(1)
 
 
-def extract_additional_data_to_df(df, workbook, sheet_index, desired_columns, queried_df_index, queried_column, backups=[], clean_datetime=False, check_previous=False, print_statements=True):
-    ### THIS FUNCTION IS USED TO ADD DATA TO EXISTING ENTRIES IN YOUR DATAFRAME; IT SEARCHES FOR MATCHES OF QUERIED DATA WITHIN THE QUERIED COLUMN OF THE WORKBOOK.
-    ### IT THEN EXTRACTS CORRESPONDING DATA FROM THE DESIRED COLUMNS AND APPENDS IT TO THE DATAFRAME.
+def extend_df_entries(df, workbook, sheet_index, desired_columns, queried_df_index, queried_column, backups=[], clean_datetime=False, check_previous=False, print_statements=True):
+    # THIS FUNCTION IS USED TO ADD DATA TO EXISTING ENTRIES IN YOUR DATAFRAME; IT SEARCHES FOR MATCHES OF QUERIED DATA WITHIN THE QUERIED COLUMN OF THE WORKBOOK.
+    # IT THEN EXTRACTS CORRESPONDING DATA FROM THE DESIRED COLUMNS AND APPENDS IT TO THE DATAFRAME.
     # df                -- the dataframe being written
     # workbook          -- the workbook being queried
     # sheet_index       -- the worksheet being queried
@@ -158,7 +166,7 @@ def extract_additional_data_to_df(df, workbook, sheet_index, desired_columns, qu
     # print_statements  -- will return print-statements outlining progress of data extraction if True (True by default)
     for (index, i) in enumerate(df):
         queried_datum = i[queried_df_index]
-        
+
         # check previous to save time
         if (check_previous == True) and (len(df[index-1]) > 0) and ((df[index-1][queried_df_index]) == queried_datum):
             entry_difference = (len(df[index-1]) - len(i))
@@ -167,19 +175,22 @@ def extract_additional_data_to_df(df, workbook, sheet_index, desired_columns, qu
                 i[(len(i)-1)] = df[index-1][(len(i)-1)]
 
         else:
-            row_number = check_row_number(workbook, sheet_index, queried_column, queried_datum)
+            row_number = check_row_number(
+                workbook, sheet_index, queried_column, queried_datum)
 
             # iterate through all desired_columns of data
             if (row_number != None):
                 for column in desired_columns:
-                    output_data = extract_datum(workbook, sheet_index, column, row_number)
-                    
+                    output_data = extract_datum(
+                        workbook, sheet_index, column, row_number)
+
                     # clean the datetime objects
                     if (clean_datetime != False):
-                        output_data = clean_datetime_object(output_data, clean_datetime)
+                        output_data = clean_datetime_object(
+                            output_data, clean_datetime)
 
                     i.append(output_data)
-        
+
             # iterate through all desired_columns of data in backup inputs
             elif (backups != []):
                 # iterate through all backups
@@ -192,38 +203,41 @@ def extract_additional_data_to_df(df, workbook, sheet_index, desired_columns, qu
                         bu_queried_column = backup[4]
                         bu_queried_datum = i[bu_queried_index]
 
-                        row_number = check_row_number(bu_workbook, bu_worksheet, bu_queried_column, bu_queried_datum)
+                        row_number = check_row_number(
+                            bu_workbook, bu_worksheet, bu_queried_column, bu_queried_datum)
 
                         # iterate through all desired_columns of data
                         if row_number != None:
                             for column in bu_desired_columns:
-                                output_data = extract_datum(bu_workbook, bu_worksheet, column, row_number)
+                                output_data = extract_datum(
+                                    bu_workbook, bu_worksheet, column, row_number)
 
                                 # clean the datetime objects
                                 if (clean_datetime != False):
-                                    output_data = clean_datetime_object(output_data, clean_datetime)
+                                    output_data = clean_datetime_object(
+                                        output_data, clean_datetime)
 
                                 i.append(output_data)
-                            break  
+                            break
                     except:
                         for column in desired_columns:
                             i.append(None)
 
             # if no matches, append blank spaces
-            else: 
+            else:
                 for column in desired_columns:
                     i.append(None)
 
         if print_statements:
             progress = ((index/len(df))*100)
             print(f"Extracting data from {workbook}: {int(progress)}% -- {i}")
-        
+
     print(f"Extracting data from {workbook}: 100%")
     return df
 
 
 def write_df_to_excel_workbook(df, workbook, print_statements=True):
-    ### THIS FUNCTION IS USED TO TRANSFER YOUR DATABASE BACK INTO MICROSOFT EXCEL
+    # THIS FUNCTION IS USED TO TRANSFER YOUR DATABASE BACK INTO MICROSOFT EXCEL
     # df                -- the dataframe being extracted from
     # workbook          -- the workbook being written
     # print_statements  -- will return print-statements outlining progress of data extraction if True (True by default)
