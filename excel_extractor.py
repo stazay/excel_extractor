ac4ad90bb7f1ddf8ecccb43f1b63dca7f2059bc7
Saba Tazayoni <61740634+stazay@users.noreply.github.com>
2022-11-01 16:20:28 +0000

    Update excel_extractor.py

--- excel_extractor.py
+++ excel_extractor.py
@@ -2,7 +2,7 @@
 excel_extractor - a basic 'relational database' tool       - Saba Tazayoni, 15/10/2021
 
     The purpose of this code is to be able to consolidate data from Excel spreadsheets.
-    A further function allows it to then form a relational database,
+    A further function allows it to use links between these spreadsheets - 
     and extract data from other spreadsheets where there is a matching point of reference.
 
     Finally, Python can then return a dataframe - which can be used with Pandas,
